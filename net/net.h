41407951b6a12fb5724180c06f8f148292c55e29
Mr.fang <fangdong@pipul.org>
2012-03-01 17:43:43 +0800

    fixup not include socket.h

--- net/net.h
+++ net/net.h
@@ -1,7 +1,7 @@
 #ifndef __NET_H_
 #define __NET_H_
 
-
+#include <sys/socket.h>
 
 int tcpConnect(const char *host, const char *serv);
 int tcpListen(const char *host, const char *serv, socklen_t *addrlen);
