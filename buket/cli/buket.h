c000894aaa2995fef49f2f9f7867be6f247aae7c
pipul <fangdong@pipul.org>
2012-03-02 21:50:46 +0800

    Update buket/cli/buket.h

--- buket/cli/buket.h
+++ buket/cli/buket.h
@@ -8,8 +8,8 @@ typedef struct FILE_t {
 	char name[];
 } BFILE;
 
-int b_read(BFILE *fp, void *buf, size_t size,...);
 BFILE *b_open(const char *host, const char *port, const char *file);
+int b_read(BFILE *fp, void *buf, size_t size,...);
 int b_close(BFILE *fp);
 
 
