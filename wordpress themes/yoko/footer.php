14bfe4579c7c4367ed59177802faa31f61319990
Mr.fang <fangdong@pipul.org>
2012-03-03 23:54:03 +0800

    wordpress theme:yoko

---
+++ wordpress themes/yoko/footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Yoko
+ */
+?>
+
+</div><!-- end wrap -->
+
+	<footer id="colophon" class="clearfix">
+		<p><a href="#page"><?php _e( '返回顶部  ', 'yoko' ); ?> </a> <?php printf( __( '主题: %1$s %2$s 由%3$s改写', 'yoko' ), 'Yoko', '<a href="http://www.elmastudio.de/wordpress-themes/">Elmastudio</a>', '<a href="http://blog.pipul.org/"> 老栋的BLOG </a>' ); ?></p>
+	</footer><!-- end colophon -->
+	
+</div><!-- end page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wordpress themes/yoko/sidebar.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Yoko
+ */
+?>
+
+<div id="secondary" class="widget-area" role="complementary">
+			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
+				
+			<?php endif; // end sidebar 1 widget area ?>
+		</div><!-- #secondary .widget-area -->
+</div><!-- end main -->
