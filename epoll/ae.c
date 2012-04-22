a13b2359312749ecc7eef163e530b1bee5d1286d
老栋 <fangdong@pipul.org>
2012-04-22 18:40:53 +0800

    change the function interface

--- epoll/ae.c
+++ epoll/ae.c
@@ -1,197 +1,66 @@
-#include <stdio.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <string.h>
+#ifndef __AE_H_
+#define __AE_H_
 
-#include "ae.h"
+#define AE_SETSIZE (1024*10)	/* Max number of fd supported */
+#define AE_OK 0
+#define AE_ERR -1
 
-EL *el_open() {
-	EL *el;
-	int i;
+#define AE_NONE 0
+#define AE_READABLE 1
+#define AE_WRITABLE 2
 
-	if (!(el = malloc(sizeof(EL))))
-		return(NULL);
-	/* 1024 is just an hint for the kernel */
-	el->efd = epoll_create(1024);
-	el->stop = 0;
+#include <sys/epoll.h>
 
-	/*
-	 * Events with mask == AE_NONE are not set.
-	 * So let's initialize the vector with it.
-	 */
-	for (i = 0;i < AE_SETSIZE;i++)
-		el->ev[i].mask = AE_NONE;
+struct eloop;
 
-	return(el);
-}
-
-int el_close(EL *el)
-{
-	close(el->efd);
-	free(el);
-
-	return(AE_OK);
-}
-
-int el_stop(EL *el) {
-	el->stop = 1;
-	return(AE_OK);
-}
-
-int el_start(EL *el, int flags) {
-	int readyEventsNum;
-	el->stop = 0;
-	while (!el->stop) {
-		struct timeval tv, *tvp;
-
-		/*
-		 * Calculate the time missing for the epoll_wait().
-		 * if we have to check for events but need to return
-		 * because of AE_NO_BLOCK. so we can set the timeout
-		 * to zero.
-		 */
-		if (flags & AE_NO_BLOCK) {
-			tv.tv_sec = tv.tv_usec = 0;
-			tvp = &tv;
-		} else {
-			tvp = NULL;
-		}
-
-		readyEventsNum = epoll_wait(
-			el->efd, el->rd, AE_SETSIZE, tvp ? (
-				tvp->tv_sec*1000 + tvp->tv_usec/1000): -1);
-		readyEventsNum = readyEventsNum > 0 ? readyEventsNum : 0;
-		
-		int j;
-		for (j = 0;j < readyEventsNum;j++) {
-			struct epoll_event *e = el->rd + j;
-			int mask;
-			efile_t *fe = &el->ev[e->data.fd];
-
-			if (e->events & EPOLLIN)
-				mask |= AE_READABLE;
-			if (e->events & EPOLLOUT)
-				mask |= AE_WRITABLE;
-
-			/*
-			 * Note the fe->mask & mask &... code:
-			 * Maybe an already processed event removed an
-			 * element that fired and we still didn't precessed
-			 * , so we check if the event is still valid.
-			 */
-			int rf = 0;
-			if (fe->mask & mask & AE_READABLE) {
-				rf = 1;
-				fe->rProc(el,e->data.fd,fe->data,mask);
-			}
-			if (fe->mask & mask & AE_WRITABLE) {
-				if(!rf || fe->wProc != fe->rProc)
-					fe->wProc(el,
-						e->data.fd,fe->data,mask);
-			}
-		}
-	}
-
-	return(AE_OK);
-}
+/* Call back function */
+typedef void cbfunc(struct eloop *el, int32_t fd, void *argv, int32_t mask);
 
 
-int el_addFileEvent(el, fd, mask, proc, data)
+typedef struct
+ asynchronous_event {
+	int32_t mask;
+	cbfunc 	*rcb;	/* read operation call back func */
+	cbfunc 	*wcb;	/* write operation call back func */
+	void 	*data;
+} ae_t;
 
-EL *el;
-int fd;
-int mask;
-eProc *proc;
-void *data;
+typedef struct eloop {
+	int32_t efd;
+	int32_t state;
+#define AE_RUN  1
+#define AE_STOP	2
+	ae_t	events[AE_SETSIZE];
+	struct epoll_event ready[AE_SETSIZE];
+} EL;
 
-{
-	if (fd >= AE_SETSIZE)
-		return(AE_ERR);
-	efile_t *fe = &el->ev[fd];
+EL *el_open(void);
+int32_t el_close(EL *el);
+int32_t el_stop(EL *el);
 
-	/*
-	 * If the fd was already monitored for some event, we need a
-	 * Mod operation. Otherwise we need a Add operation.
-	 */
-	int op = (fe->mask == AE_NONE) ? EPOLL_CTL_ADD : EPOLL_CTL_MOD;
+#define AE_NO_BLOCK 1
+int32_t el_start(EL *el, int32_t flags);
 
-	/*
-	 * Merge the old events. 
-	 */
-	fe->mask |= mask;
+int32_t el_addEvent(EL *el, int32_t fd, int32_t mask, cbfunc *cb, void *data);
+int32_t el_delEvent(EL *el, int32_t fd, int32_t mask);
 
-	struct epoll_event ee;
-	ee.events = 0;
 
-	if (fe->mask != AE_NONE) {
-		if (fe->mask & AE_READABLE)
-			ee.events |= EPOLLIN;
-		if (fe->mask & AE_WRITABLE)
-			ee.events |= EPOLLOUT;
-	}
 
-	/*
-	 * Avoid valgrind warning.
-	 */
-	ee.data.u64 = 0;
-	ee.data.fd = fd;
 
-	if (epoll_ctl(el->efd,op,fd,&ee) == -1)
-		return(AE_ERR);
 
-	/*
-	 * Add the process function to the EL.
-	 */
-	fe->rProc = (mask & AE_READABLE) ? proc : NULL;
-	fe->wProc = (mask & AE_READABLE) ? proc : NULL;
-	
-	fe->data = data;
 
-	return(AE_OK);
-}
 
-int el_delFileEvent(EL *el, int fd, int mask)
-{
-	if (fd >= AE_SETSIZE)
-		return(AE_ERR);
-	efile_t *fe = &el->ev[fd];
 
-	if (!(fe->mask & mask))
-		return(AE_ERR);
-	/*
-	 * Delete the event from el.
-	 */
-	fe->mask = fe->mask & (~mask);
 
-	/*
-	 * Note that if the fd was still monitor other event, we need a
-	 * Mod operation. Otherwise we need a DEL operation.
-	 */
-	int op = (fe->mask == AE_NONE) ? EPOLL_CTL_DEL : EPOLL_CTL_MOD;
 
-	struct epoll_event ee;
-	ee.events = 0;
 
-	if (fe->mask != AE_NONE) {
-		if (fe->mask & AE_READABLE)
-			ee.events |= EPOLLIN;
-		if (fe->mask & AE_WRITABLE)
-			ee.events |= EPOLLOUT;
-	}
 
-	/*
-	 * Avoid valgrind warning
-	 */
-	ee.data.u64 = 0;
-	ee.data.fd = fd;
 
-	if (epoll_ctl(el->efd,op,fd,&ee) == -1)
-		return(AE_ERR);
 
-	fe->rProc = (mask & AE_READABLE) ? NULL : fe->rProc;
-	fe->wProc = (mask & AE_WRITABLE) ? NULL : fe->wProc;
-	fe->data = (fe->mask == AE_NONE) ? NULL : fe->data;
 
-	return(AE_OK);
-}
+
+
+
+
+
+#endif
--- epoll/ae.h
+++ epoll/ae.h
@@ -1,46 +1,146 @@
-#ifndef __AE_H_
-#define __AE_H_
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "ae.h"
 
-#define AE_SETSIZE (1024*10)	/* Max number of fd supported */
-#define AE_OK 0
-#define AE_ERR -1
+EL *el_open()
+{
+	EL *el;
+	int32_t i;
 
-#define AE_NONE 0
-#define AE_READABLE 1
-#define AE_WRITABLE 2
+	if ((el = malloc(sizeof(EL))) == NULL)
+		return(NULL);
+	el->efd = epoll_create(1024);
+	el->state = STOP;
+	for (i = 0; i < AE_SETSIZE; i++)
+		el->events[i].mask = AE_NONE;
+	return(el);
+}
 
-#define AE_NO_BLOCK 2
+int32_t *el_close(EL *el)
+{
+	close(el->efd);
+	free(el);
+	return(AE_OK);
+}
 
-#include <sys/epoll.h>
+int32_t el_stop(EL *el) {
+	el->state = STOP;
+	return(AE_OK);
+}
 
-typedef struct epoll_event efire_t;
-struct eloop;
+int32_t el_start(EL *el, int32_t flag)
+{
+	ae_t *e;
+	int32_t n,i;
+	int32_t mask,rf;
+	struct timeval tv, *tvp;
+	struct epoll_event *ee;
 
-typedef void eProc(struct eloop *el, int fd, void *argv, int mask);
+	el->state = AE_RUN;	
+	while (el->state != AE_STOP) {
+		if (flag & AE_NO_BLOCK) {
+			tv.tv_sec = tv.tv_usec = 0;
+			tvp = &tv;
+		} else {
+			tvp = NULL;
+		}
+		n = epoll_wait(el->efd, el->ready, AE_SETSIZE,
+		 tvp ? (tvp->tv_sec*1000 + tvp->tv_usec/1000) : -1);
+		n = n > 0 ? n : 0;
+		
+		for (i = 0; i < n; i++) {
+			ee = el->ready + i;
+			e = &el->events[ee->data.fd];
+			if (ee->events & EPOLLIN)
+				mask = mask | AE_READABLE;
+			if (ee->events & EPOLLOUT)
+				mask = mask | AE_WRITABLE;
+			if (e->mask & mask & AE_READABLE) {
+				rf = 1;
+				e->rcb(el,ee->data.fd,e->data,mask);
+			}
+			if (e->mask & mask & AE_WRITABLE)
+				if (rf != 1 || e->rcb != e->wcb)
+					e->wcb(el,ee->data.fd,e->data,mask);
+		}
+	}
+	return(AE_OK);
+}
 
-/* File event structure */
-typedef struct efile {
-	int mask;
-	eProc *rProc;
-	eProc *wProc;
-	void *data;
-} efile_t;
+int32_t el_addEvent(
+	EL *el, int32_t fd, int32_t mask, cbfunc *cb, void *data)
+{
+	ae_t e;
+	int32_t op;
+	struct epoll_event *ee;
 
-typedef struct eloop {
-	int efd;
-	int stop;
-	efile_t ev[AE_SETSIZE];
-	efire_t rd[AE_SETSIZE];
-} EL;
+	if (fd >= AE_SETSIZE)
+		return(AE_ERR);
+	e = &el->events[fd];
+	op = (e->mask == AE_NONE) ? EPOLL_CTL_ADD : EPOLL_CTL_MOD;
+	e->mask = e->mask | mask;
+	ee.events = 0;
+	
+	if (e->mask != AE_NONE) {
+		if (e->mask & AE_READABLE)
+			ee.events |= EPOLLIN;
+		if (e->mask & AE_WRITABLE)
+			ee.events |= EPOLLOUT;
+	}
+	
+	ee.data.u64 = 0;
+	ee.data.fd = fd;
+	
+	if (-1 == epoll_ctl(el->efd,op,fd,&ee))
+		return(AE_ERR);
+	e->rcb = (mask & AE_READABLE) ? cb : NULL;
+	e->wcb = (mask & AE_WRITABLE) ? cb : NULL;
+	e->data = data;
+	
+	return(AE_OK);
+}
 
-/* prototypes */
 
-EL *el_open(void);
-int el_close(EL *el);
-int el_stop(EL *el);
-int el_start(EL *el, int flags);
+int32_t el_delEvent(EL *el, int32_t fd, int32_t mask)
+{
+	ae_t e;
+	int32_t op;
+	struct epoll_event ee;
 
-int el_addFileEvent(EL *el, int fd, int mask, eProc *pro, void *data);
-int el_delFileEvent(EL *el, int fd, int mask);
+	if (fd >= AE_SETSIZE)
+		return(AE_ERR);
+	e = &el->events[fd];
+	if (!(e->mask & mask))
+		return(AE_ERR);
+	e->mask = e->mask & (~mask);
+	op = (e->mask == AE_NONE) ? EPOLL_CTL_DEL : EPOLL_CTL_MOD;
+	ee.events = 0;
+	
+	if (e->mask != AE_NONE) {
+		if (e->mask & AE_READABLE)
+			ee.events |= EPOLLIN;
+		if (e->mask & AE_WRITABLE);
+			ee.events |= EPOLLOUT;
+	}
+	
+	ee.data.u64 = 0;
+	ee.data.fd = fd;
+	
+	if (-1 == epoll_ctl(el->efd,op,fd,&ee))
+		return(AE_ERR);
+	e->rcb = (mask & AE_READABLE) ? NULL : e->rcb;
+	e->wcb = (mask & AE_WRITABLE) ? NULL : e->wcb;
+	e->data = (e->mask == AE_NONE) ? NULL : e->data;
+	
+	return(AE_OK);
+}
 
-#endif
+
+
+
+
+
+
+
+
